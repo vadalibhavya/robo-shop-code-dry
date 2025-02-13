09f0895531f08927771156c50b74e6a1a5418402
avadaliDevOps <adityavittal92@gmail.com>
2025-02-13 12:38:16 +0530

    Update

--- common.sh
+++ common.sh
@@ -1,6 +1,11 @@
+# This function is used to set up the systemd service for the given component.
+# It reloads the systemctl daemon, enables the component and starts it.
 systemd_setup() {
+  # Reload the systemctl daemon to pick up the new service file.
   systemctl daemon-reload
+  # Enable the component to start on boot.
   systemctl enable $component
+  # Start the component.
   systemctl start $component
 }
 
