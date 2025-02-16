c396be4e4122750b74fe8b4083690a586e3118cf
avadaliDevOps <adityavittal92@gmail.com>
2025-02-16 12:25:07 +0530

    Update

--- catalogue.sh
+++ catalogue.sh
@@ -1,7 +1,5 @@
 source common.sh
 component=catalogue
-print_message "Installing $component"
 node_setup
-print_message "Installing $component"
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.doubtfree.online </app/db/master-data.js
--- common.sh
+++ common.sh
@@ -1,15 +1,27 @@
 # This function is used to set up the systemd service for the given component.
 # It reloads the systemctl daemon, enables the component and starts it.
 systemd_setup() {
+  print_message "Setting up systemd service for $component"
   systemctl daemon-reload && systemctl enable $component && systemctl start $component
 }
 
 artifact_download() {
-  rm -rf /app && mkdir /app && curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component-v3.zip && cd /app && unzip /tmp/$component.zip
+  print_message "Downloading $component artifact"
+  rm -rf /app && mkdir /app &&
+  curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component-v3.zip
+  print_message "Extracting $component artifact"
+  && cd /app
+  && unzip /tmp/$component.zip
 }
 
 node_setup() {
-  dnf module disable nodejs -y && dnf module enable nodejs:20 -y && cp $component.service /etc/systemd/system/$component.service && dnf install nodejs -y && useradd roboshop && artifact_download && cd /app && npm install && systemd_setup
+  print_message "Installing $component"
+  dnf module disable nodejs -y &&  print_message enabling the nodejs &&
+  dnf module enable nodejs:20 -y && print_message Copying the service file &&
+  cp $component.service /etc/systemd/system/$component.service &&
+  dnf install nodejs -y &&
+  useradd roboshop && artifact_download &&
+  cd /app && npm install && systemd_setup
 }
 
 print_message() {
